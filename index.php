d641205092df2a4da72fb1c6798cde3d067d08f7
Jason <jasonmm.github@gmail.com>
2015-07-03 19:46:47 -0500

    Changed the main form to use Twig templates.

--- index.php
+++ index.php
@@ -9,15 +9,25 @@ use Jasonmm\WordSearch\WordSearch;
 require_once 'vendor/autoload.php';
 require_once('classes.inc.php');
 
-$page_title = 'Create Word Search';
-require_once('html_top.tpl.html');
-
 $ws = new WordSearch();
 if( isset($_FILES['wordsearch_file']) ) {
     require_once('load_wordsearch.php');
 }
 
-require_once('create_wordsearch.tpl.html');
+$loader = new Twig_Loader_Filesystem('templates');
+$twig = new Twig_Environment($loader, array());
 
-require_once('html_bottom.tpl.html');
+$params = [
+    'ws' => $ws,
+    'isCreated' => $ws->IsCreated(),
+    'wordList' => $ws->GetWordList("\n"),
+    'wordSearchTitle' => $ws->GetTitle(),
+    'wordSearchRows' => $ws->GetRows(),
+    'wordSearchCols' => $ws->GetCols(),
+    'wordListSortBy' => $ws->GetSortBy(),
+    'wordSearchShowDate' => $ws->GetShowDate(),
+    'wordSearchUppercase' => $ws->GetWordsInUppercase(),
+    'wordSearchObj' => base64_encode(gzcompress(serialize($ws), 9)),
+];
+echo $twig->render('create-wordsearch.twig', $params);
 
